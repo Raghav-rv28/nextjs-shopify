fa4744c2643fa9b1ba481d605fd9d7c6bc5b917a
Raghav Rudhra <raghavrudhra28@gmail.com>
2024-01-21 00:28:16 -0500

    hotfix

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -11,6 +11,8 @@ import { getProduct, getProductRecommendations } from 'lib/shopify';
 import { Image } from 'lib/shopify/types';
 import Link from 'next/link';
 
+export const runtime = 'edge';
+
 export async function generateMetadata({
   params
 }: {
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,6 +8,7 @@ export const metadata = {
   description: 'Search for products in the store.'
 };
 
+export const runtime = 'edge';
 export default async function SearchPage({
   searchParams
 }: {
