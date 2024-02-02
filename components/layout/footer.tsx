be7e3b8122539f1b2a57c2c7a61fc1d643bd49fd
Raghav Rudhra <raghavrudhra28@gmail.com>
2024-02-01 21:20:19 -0500

    adding social icons in footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,16 +1,10 @@
 import FooterMenu from 'components/layout/footer-menu';
 import { getMenu } from 'lib/shopify';
-import {
-  FacebookIcon,
-  InstagramIcon,
-  LocateIcon,
-  MailIcon,
-  PhoneIcon,
-  PlusIcon,
-  ShareIcon
-} from 'lucide-react';
+import { FacebookIcon, InstagramIcon, LocateIcon, MailIcon, PhoneIcon } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
+import { ImYelp } from 'react-icons/im';
 
 export default async function Footer() {
   const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700';
@@ -37,10 +31,27 @@ export default async function Footer() {
               </p>
               <p className="mb-2 text-xl font-semibold uppercase text-gray-600">Follow Us</p>
               <div className="flex space-x-2">
-                <FacebookIcon className="h-6 w-6 text-blue-600" />
-                <InstagramIcon className="h-6 w-6 text-pink-500" />
-                <PlusIcon className="h-6 w-6 text-red-600" />
-                <ShareIcon className="h-6 w-6 text-gray-600" />
+                <Link href={'https://www.facebook.com/jewellersdubai'} target="_blank">
+                  <FacebookIcon className="h-6 w-6 text-blue-600" />
+                </Link>
+                <Link href={'https://www.instagram.com/dubai.jewellers/'} target="_blank">
+                  <InstagramIcon className="h-6 w-6 text-pink-500" />
+                </Link>
+                <Link href={'https://g.co/kgs/9YN3jQH'} target="_blank">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="25"
+                    height="25"
+                    viewBox="0 0 50 50"
+                  >
+                    <path d="M 17.1875 10.9375 C 9.421875 10.9375 3.125 17.234375 3.125 25 C 3.125 32.765625 9.421875 39.0625 17.1875 39.0625 C 24.953125 39.0625 31.25 32.765625 31.25 25 C 31.25 24.035156 31.144531 23.09375 30.960938 22.1875 L 30.882813 21.875 L 17.1875 21.875 L 17.1875 26.5625 L 26.5625 26.5625 C 25.816406 30.996094 21.832031 34.375 17.1875 34.375 C 12.007813 34.375 7.8125 30.179688 7.8125 25 C 7.8125 19.820313 12.007813 15.625 17.1875 15.625 C 19.53125 15.625 21.667969 16.492188 23.3125 17.914063 L 26.671875 14.625 C 24.171875 12.335938 20.84375 10.9375 17.1875 10.9375 Z M 39.0625 17.1875 L 39.0625 21.875 L 34.375 21.875 L 34.375 25 L 39.0625 25 L 39.0625 29.6875 L 42.1875 29.6875 L 42.1875 25 L 46.875 25 L 46.875 21.875 L 42.1875 21.875 L 42.1875 17.1875 Z"></path>
+                  </svg>
+                </Link>
+                <Link href={'https://www.yelp.ca/biz/dubai-jewellers-brampton'} target="_blank">
+                  <ImYelp className="h-6 w-6" />
+                </Link>
               </div>
             </div>
             <div>
